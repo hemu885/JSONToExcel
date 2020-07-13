86f2a3d4d2c9c2faf7a9cf1326e00a8fb9fd186d
H Chaudhary <hemu1885@gmail.com>
2020-07-13 19:53:18 +0530

    Committed first of XOM

---
+++ MyHRAttendance-XOM/src/com/My/HR/output/AttendnaceMessage.java
@@ -0,0 +1,28 @@
+package com.My.HR.output;
+
+public class AttendnaceMessage {
+	
+	private String messageType;
+	private String  messageInfo;
+	
+	public AttendnaceMessage(){
+		
+	}
+	
+	public String getMessageType() {
+		return messageType;
+	}
+	public void setMessageType(String messageType) {
+		this.messageType = messageType;
+	}
+	public String getMessageInfo() {
+		return messageInfo;
+	}
+	public void setMessageInfo(String messageInfo) {
+		this.messageInfo = messageInfo;
+	}
+
+	
+	
+
+}
